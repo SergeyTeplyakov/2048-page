49811686ebfe4ddd8ba319f20baa571edfce886a
Sergey Teplyakov <Sergey.Teplyakov@gmail.com>
2015-09-11 19:44:45 -0700

    Initial version

---
+++ Scripts/state.ts
@@ -0,0 +1,41 @@
+﻿module Model {
+    
+    /**
+     * Represents current game state.
+     */
+    export interface GameSnapshot {
+        firstPlayer: Tile;
+        nextPlayer: Tile;
+
+        gameStatus: GameStatus;
+        winner: Tile;
+
+        grid: {
+            size: number;
+            longestStrike: number;
+            cells: Tile[][];
+        }
+    }
+
+    export interface GameStatistics {
+        firstPlayerScore: number;
+        secondPlayerScore: number;        
+    }
+
+    export enum GameStatus {
+        Draw,
+        Victory,
+        KeepPlaying,
+    }
+
+    export class Draw { }
+
+    export class KeepPlaying { }
+
+    export class Victory {
+        constructor(public winner: Tile) { }
+    }
+
+    export type MoveResult = KeepPlaying | Draw | Victory;
+
+}
