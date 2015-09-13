40bd0425b057305a2df2366027cfdcfffdfa028d
Sergey Teplyakov <Sergey.Teplyakov@gmail.com>
2015-09-13 11:55:13 -0700

    fixes

--- fiveinarow/Scripts/application.js
+++ fiveinarow/Scripts/application.js
@@ -3,8 +3,8 @@ window.requestAnimationFrame(function () {
     var firstPlayer = "Player 1";
     var secondPlayer = "Player 2";
     var view = new View.GameView(firstPlayer, secondPlayer);
-    var gridSize = 3;
-    var longestStrike = 3;
+    var gridSize = 10;
+    var longestStrike = 5;
 
     new Control.GameController(gridSize, longestStrike, view, undefined, firstPlayer, secondPlayer);
 });
